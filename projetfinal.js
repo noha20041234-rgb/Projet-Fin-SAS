add09be1d3755814f679e1b541fb9c487ff0eaf1
Noha <noha20041234@gmail.com>
2026-09-25 13:01:26 +0200

    Update projet final

--- projetfinal.js
+++ projetfinal.js
@@ -1,76 +1,96 @@
 const prompt = require("prompt-sync")();
 let choix;
 let candidats = [];
-function Ajoutercadidat{
-    let CIN =Number(prompt("CIN : "));
+function Ajoutercadidat() {
+    let cin = Number(prompt("CIN : "));
     let exist = false;
-    for(let i = 0; i < candidats.length ; i++){
-        if(candidats[i].CIN === CIN){
+    for (let i = 0; i < candidats.length; i++) {
+        if (candidats[i].cin === cin) {
             exist = true;
         }
     }
-    if(exist){
-        console.log("ce cadidat exist deja. ");
-        return
-}
-    let cin = prompt("CIN : ");
+    if (exist) {
+        console.log("Ce candidat existe deja.");
+        return;
+    }
     let nom = prompt("Nom : ");
     let prenom = prompt("Prénom : ");
     let partiPolitique = prompt("Parti politique : ");
     let age = Number(prompt("Age : "));
     let candidat = {
-        cin : cin ;
-        nom : nom ;
-        prenom : prenom ;
-        partiPolitique : partiPolitique;
-        age : age;
+        cin: cin,
+        nom: nom,
+        prenom: prenom,
+        partiPolitique: partiPolitique,
+        age: age,
         electeurs: []
     };
-    codidats.push(candidat);
+
+    candidats.push(candidat);
     console.log("Candidat ajouté avec succès.");
 }
-function voter{
-    let cinelecteur = Number(prompt(" ajouter votre cin : "));
-    //// Vérifier si l'électeur a déjà voté
-    for(let i = 0; i < candidats.length; i++){
-        for(let j = 0; j < candidats.electeurs.length;j++){
-            if(candidats[i].electeurs[j] === cinelecteur){
-                console.log("vous avez deja vote. ")
+function voter() {
+    let cinelecteur = Number(prompt("Ajouter votre CIN : "));
+    // Vérifier si l'électeur a déjà voté
+    for (let i = 0; i < candidats.length; i++) {
+        for (let j = 0; j < candidats[i].electeurs.length; j++) {
+            if (candidats[i].electeurs[j] === cinelecteur) {
+                console.log("Vous avez deja vote.");
                 return;
             }
         }
     }
     // Demander la CIN du candidat
-    let cincadidat = Number(prompt(" CIN du candidat : "));
-    //Chercher le candidat
-    for(let i = 0; i < candidats.length; i++){
-        if(candidats[i].cin === cincadidat){
-            candidat[i].electeurs.push(candidats);
+    let cincandidat = Number(prompt("CIN du candidat : "));
+    // Chercher le candidat
+    for (let i = 0; i < candidats.length; i++) {
+        if (candidats[i].cin === cincandidat) {
+            candidats[i].electeurs.push(cinelecteur);
             console.log("Vote enregistré");
-                return;
+            return;
         }
     }
-    // le candidat n'exite pas 
-console.log("Candidat introuvable.");
+    // Le candidat n'existe pas
+    console.log("Candidat introuvable.");
 }
-do{
-    console.log("===== Gestion d'une campagne électorale =====;");
-    console.log ("1. Ajouter un nouveau candidat :  ");
-    console.log("2. Ajouter plusieurs candidats à la fois : ");
-    choix = Number(prompt("votre choix : "));
-    switch (choix){
-        case 1 :
+function Afficher() {
+    // deux choix selon
+    let choix2 = Number(prompt("1 selon le nombre de vote : "));
+
+    // selon le nombre de vote
+    switch (choix2) {
+        case 1:
+            for (let i = 0; i < candidats.length; i++) {
+
+            }
+            break;
+    }
+
+    // 2 selon le parti politique
+}
+do {
+    console.log("===== Gestion d'une campagne électorale =====");
+    console.log("1. Ajouter un nouveau candidat");
+    console.log("2. Ajouter plusieurs candidats à la fois");
+    console.log("3. Voter pour un candidat");
+    console.log("4. Afficher la liste des candidats");
+    choix = Number(prompt("Votre choix : "));
+    switch (choix) {
+        case 1:
             Ajoutercadidat();
             break;
-        case 2 :
-            let Number = Number(prompt("Combien de candidats voulez-vous ajouter ? "));
-            for(let i = 0; i < Number,i++){
+        case 2:
+            let nombre = Number(prompt("Combien de candidats voulez-vous ajouter ? "));
+
+            for (let i = 0; i < nombre; i++) {
                 Ajoutercadidat();
+            } 
             break;
-            }
-        case 3 :
-            // voter 
-             break;
+        case 3:
+            voter();
+            break;
+        case 4:
+            Afficher();
+            break;
     }
-    
 } while (choix !== 0);
